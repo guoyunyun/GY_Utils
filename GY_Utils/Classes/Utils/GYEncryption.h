a959e0f27adf333635d901a6ef00a9aafbf7bf9f
Yong <953576191@qq.com>
2019-07-19 14:43:42 +0800

    [*]添加des

--- GY_Utils/Classes/Utils/GYEncryption.h
+++ GY_Utils/Classes/Utils/GYEncryption.h
@@ -12,12 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GYEncryption : NSObject
 
+#pragma mark md5
+
 //  32位小写
 + (nullable NSString *)md5StringWithString:(NSString *)string;
 
 //  32位大写
 + (nullable NSString *)MD5StringWithString:(NSString *)string;
 
+#pragma mark des
+
+//加密
++ (NSString *) encryptUseDES2:(NSString *)plainText key:(NSString *)key;
+
+//解密
++ (NSString *)decryptUseDES:(NSString *)cipherText key:(NSString *)key;
 
 
 
--- GY_Utils/Classes/Utils/GYEncryption.m
+++ GY_Utils/Classes/Utils/GYEncryption.m
@@ -44,4 +44,76 @@
 }
 
 
+
+//加密
++ (NSString *) encryptUseDES2:(NSString *)plainText key:(NSString *)key{
+
+    NSString *ciphertext = nil;
+    const char *textBytes = [plainText UTF8String];
+    NSUInteger dataLength = [plainText length];
+    unsigned char buffer[1024];
+    memset(buffer, 0, sizeof(char));
+    Byte iv[] = {1,2,3,4,5,6,7,8};
+    size_t numBytesEncrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt, kCCAlgorithmDES,
+                                          kCCOptionPKCS7Padding|kCCOptionECBMode,
+                                          [key UTF8String], kCCKeySizeDES,
+                                          iv,
+                                          textBytes, dataLength,
+                                          buffer, 1024,
+                                          &numBytesEncrypted);
+    if (cryptStatus == kCCSuccess) {
+        NSData *data = [NSData dataWithBytes:buffer length:(NSUInteger)numBytesEncrypted];
+
+        ciphertext = [self encodeData:data];
+    }
+
+    return ciphertext;
+}
+
+//解密
++ (NSString *)decryptUseDES:(NSString *)cipherText key:(NSString *)key
+{
+    NSData* cipherData = [self decodeString:cipherText];
+    unsigned char buffer[1024];
+    memset(buffer, 0, sizeof(char));
+    size_t numBytesDecrypted = 0;
+    Byte iv[] = {1,2,3,4,5,6,7,8};
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,
+                                          kCCAlgorithmDES,
+                                          kCCOptionPKCS7Padding|kCCOptionECBMode,
+                                          [key UTF8String],
+                                          kCCKeySizeDES,
+                                          iv,
+                                          [cipherData bytes],
+                                          [cipherData length],
+                                          buffer,
+                                          1024,
+                                          &numBytesDecrypted);
+    NSString* plainText = nil;
+    if (cryptStatus == kCCSuccess) {
+        NSData* data = [NSData dataWithBytes:buffer length:(NSUInteger)numBytesDecrypted];
+        plainText = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    return plainText;
+
+}
+
++ (NSString *)encodeData:(NSData *)originData{
+
+    NSString* encodeResult = [originData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
+    //    NSString* encodeResult = [originData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
+    return encodeResult;
+}
+
++ (NSData *)decodeString:(NSString *)encodeResult{
+
+    NSData* decodeData = [[NSData alloc] initWithBase64EncodedString:encodeResult options:0];
+    //    NSString* decodeStr = [[NSString alloc] initWithData:decodeData encoding:NSASCIIStringEncoding];
+    return decodeData;
+
+}
+
+
+
 @end
